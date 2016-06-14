4b6c2923f47fc326378d6210d3f6b07aee4dc28e
alex231995 <alexfcdm01@hotmail.fr>
2016-06-14 09:35:10 +0200

    Projet+Graphique

---
+++ Projet+Graphique/src/Textes.hpp
@@ -0,0 +1,37 @@
+#ifndef TEXTES_HPP_
+#define TEXTES_HPP_
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <string>
+#include <iostream>
+
+class Textes{
+public:
+    Sprite sprite;
+    Text txt;
+
+  Textes(float xPosition, float yPosition, int i, int t){
+        sprite.setPosition(xPosition, yPosition);
+        sprite.setTextureRect(IntRect(0, 0, 32, 32));
+
+        
+	txt.setString((char)(i));
+	txt.setCharacterSize(t);
+	txt.setColor(Color::White);
+  }
+
+void setPosition(float xPosition, float yPosition){
+  sprite.setPosition(xPosition, yPosition);
+}
+
+void update(){
+  txt.setPosition(sprite.getPosition());
+}
+  
+};
+
+#endif
+  
+
+  
