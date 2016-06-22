14f7a755d084ed0b5fadafedde00a39b7bd17356
TeamUnoDeLaVega <anthonyy59@hotmail.fr>
2016-06-22 20:37:16 +0200

    Suppression du mauvais projet

--- UnoProjet/UnoClient/src/FonctionTest.hpp
+++
@@ -1,9 +0,0 @@
-#include "Carte.hpp"
-#include "Joueur.hpp"
-BOOST_AUTO_TEST_CASE(Module1_test1) { // test unitaire
-	std::string msg="44j";
-	Carte c=c.stringToCarte(msg);
-	int val=c.getValeur();
-	std::string couleur=c.getCouleur();
-	BOOST_CHECK_EQUAL(val, 44);
-}
